726fc5fa16854d97b6899f220ce85eadb629bcaa
JRaams <47593569+JRaams@users.noreply.github.com>
2021-12-03 19:09:55 +0100

    setup cli arguments

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.info('testa');
+import fs from 'fs';
+import path from 'path';
+
+// Check there are 2 arguments (besides node and executable)
+const args = process.argv;
+if (args.length < 4) {
+  console.error('');
+  console.error('Usage: node index <day> <part = [a|b]>');
+  console.error('Example: node index 1 a');
+  console.error('');
+  process.exit(1);
+}
+
+// Check first argument is a real day
+const dayDirPath = path.join(__dirname, `./calendar/${args[2]}`);
+if (!fs.existsSync(dayDirPath)) {
+  console.error('');
+  console.error(`Day '${args[2]}' was not found on the calendar...`);
+  console.error('');
+  process.exit(1);
+}
+
+// Check second arcument is 'a' or 'b'
+if (!['a', 'b'].includes(args[3])) {
+  console.error('');
+  console.error(`Part '${args[3]}' is invalid. Use 'a' or 'b'.`);
+  console.error('');
+  process.exit(1);
+}
+
+console.info('Solving day ' + args[2] + ' part ' + args[3]);
