1593b7f83b890ba6dd3d2632143c9c461946b983
JRaams <47593569+JRaams@users.noreply.github.com>
2021-12-03 20:41:46 +0100

    day 2

---
+++ src/calendar/2/index.ts
@@ -0,0 +1,50 @@
+import CalendarDay from '../calendarDay';
+
+export default class Day1 extends CalendarDay {
+  commandToPos: any = {
+    forward: { x: 1, y: 0 },
+    down: { x: 0, y: -1 },
+    up: { x: 0, y: 1 },
+  };
+
+  public solveA(): number {
+    let xSum = 0;
+    let ySum = 0;
+
+    this.lines.forEach((line) => {
+      const { x, y } = this.lineToXY(line);
+      xSum += x;
+      ySum -= y;
+    });
+
+    return xSum * ySum;
+  }
+
+  public solveB(): number {
+    let xSum = 0;
+    let ySum = 0;
+    let aim = 0;
+
+    this.lines.forEach((line: string) => {
+      const [command, valueStr] = line.split(' ');
+      let value = Number(valueStr);
+
+      if (command == 'down') {
+        aim -= value;
+      } else if (command == 'up') {
+        aim += value;
+      } else if (command == 'forward') {
+        xSum += value;
+        ySum -= aim * value;
+      }
+    });
+
+    return xSum * ySum;
+  }
+
+  lineToXY(line: string) {
+    const [command, value] = line.split(' ');
+    const { x, y } = this.commandToPos[command];
+    return { x: x * Number(value), y: y * Number(value) };
+  }
+}
