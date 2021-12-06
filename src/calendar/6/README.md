e19ba81f58e449c2f1f7235e79054e27ba49a2fd
JRaams <47593569+JRaams@users.noreply.github.com>
2021-12-06 10:20:58 +0100

    day 6 b

--- src/calendar/6/README.md
+++ src/calendar/6/README.md
@@ -55,3 +55,13 @@ In this example, after 18 days, there are a total of 26 fish. After 80 days, the
 Find a way to simulate lanternfish. **How many lanternfish would there be after 80 days?**
 
 359344
+
+--- Part Two ---
+
+Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+After 256 days in the example above, there would be a total of 26984457539 lanternfish!
+
+How many lanternfish would there be after 256 days?
+
+1629570219571
--- src/calendar/6/index.ts
+++ src/calendar/6/index.ts
@@ -3,24 +3,54 @@ import CalendarDay from '../calendarDay';
 export default class Day6 extends CalendarDay {
   public solveA(): number {
     const fish = this.lines[0].split(',').map(Number);
-
-    for (let day = 0; day < 80; day++) {
-      for (let fishIdx = fish.length - 1; fishIdx >= 0; fishIdx--) {
-        // Subtract a day
-        fish[fishIdx]--;
-
-        // If reached 0 days, reset and spawn child
-        if (fish[fishIdx] < 0) {
-          fish[fishIdx] = 6;
-          fish.push(8);
-        }
-      }
-    }
-
-    return fish.length;
+    return this.simulate(fish, 80);
   }
 
   public solveB(): number {
-    return 6.2;
+    const fish = this.lines[0].split(',').map(Number);
+    return this.simulate(fish, 256);
+  }
+
+  private simulate(fish: number[], days: number) {
+    const fishCycles: any = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+    };
+
+    fish.forEach((f: number) => {
+      fishCycles[f]++;
+    });
+
+    for (let day = 0; day < days; day++) {
+      let newSpawnCount = 0;
+      let resetCount = 0;
+
+      Object.keys(fishCycles).forEach((key: string) => {
+        if (key === '0') {
+          resetCount += fishCycles[0];
+          newSpawnCount += fishCycles[0];
+          fishCycles[0] = 0;
+        } else {
+          fishCycles[Number(key) - 1] += fishCycles[key];
+          fishCycles[key] = 0;
+        }
+      });
+
+      fishCycles[6] += resetCount;
+      fishCycles[8] += newSpawnCount;
+    }
+
+    const count = Object.values(fishCycles)
+      .map(Number)
+      .reduce((total, current) => total + current);
+
+    return count;
   }
 }
