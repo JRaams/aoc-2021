078976681a044dec420cb632a656d4abf04af303
JRaams <47593569+JRaams@users.noreply.github.com>
2021-12-06 10:31:13 +0100

    refactor 6 b

--- src/calendar/6/index.ts
+++ src/calendar/6/index.ts
@@ -12,45 +12,18 @@ export default class Day6 extends CalendarDay {
   }
 
   private simulate(fish: number[], days: number) {
-    const fishCycles: any = {
-      0: 0,
-      1: 0,
-      2: 0,
-      3: 0,
-      4: 0,
-      5: 0,
-      6: 0,
-      7: 0,
-      8: 0,
-    };
+    const school: number[] = Array(9).fill(0);
 
     fish.forEach((f: number) => {
-      fishCycles[f]++;
+      school[f]++;
     });
 
     for (let day = 0; day < days; day++) {
-      let newSpawnCount = 0;
-      let resetCount = 0;
-
-      Object.keys(fishCycles).forEach((key: string) => {
-        if (key === '0') {
-          resetCount += fishCycles[0];
-          newSpawnCount += fishCycles[0];
-          fishCycles[0] = 0;
-        } else {
-          fishCycles[Number(key) - 1] += fishCycles[key];
-          fishCycles[key] = 0;
-        }
-      });
-
-      fishCycles[6] += resetCount;
-      fishCycles[8] += newSpawnCount;
+      const first: number = school.shift()!;
+      school.push(first);
+      school[6] += school[8];
     }
 
-    const count = Object.values(fishCycles)
-      .map(Number)
-      .reduce((total, current) => total + current);
-
-    return count;
+    return school.reduce((total, current) => total + current);
   }
 }
