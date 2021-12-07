1469c3125f68ee16a24d87e2fad8e8fd668feac3
JRaams <47593569+JRaams@users.noreply.github.com>
2021-12-07 16:41:45 +0100

    day 7 part b

--- src/calendar/7/index.ts
+++ src/calendar/7/index.ts
@@ -5,7 +5,7 @@ export default class Day7 extends CalendarDay {
     const input = this.lines[0].split(',').map(Number);
     input.sort((a, b) => a - b);
 
-    // Get target position where each crab is going to move towards
+    // Get target position where each crab is going to move towards (median)
     const targetIdx = input.length / 2 - 1;
     const target = input[targetIdx];
 
@@ -15,6 +15,19 @@ export default class Day7 extends CalendarDay {
   }
 
   public solveB(): number {
-    return 7.2;
+    const input = this.lines[0].split(',').map(Number);
+    input.sort((a, b) => a - b);
+
+    // Get target position where each crab is going to move towards (average)
+    const positionSum = input.reduce((total, current) => total + current);
+    const target = Math.floor(positionSum / input.length);
+
+    // Move all crabs towards the target
+    const totalFuel = input.reduce((total, current) => {
+      let cost = Math.abs(current - target);
+      cost = 0.5 * cost * (cost + 1); // Triangular number function (thanks ALGA)
+      return total + cost;
+    }, 0);
+    return totalFuel;
   }
 }
