cf56477c69a5ef5654784b9c67c6953f3d38b0b7
JRaams <47593569+JRaams@users.noreply.github.com>
2021-12-13 22:20:30 +0100

    day 12 b lidl

--- src/calendar/12/index.ts
+++ src/calendar/12/index.ts
@@ -15,14 +15,35 @@ export default class Day12 extends CalendarDay {
   }
 
   public solveB(): number {
-    const graph = new Graph();
+    const edges: any = {};
 
     this.lines.forEach((line: string) => {
-      const [cave1, cave2] = line.split('-');
-      graph.addEdge(cave1, cave2);
+      const [a, b] = line.split('-');
+      edges[a] = [...(edges[a] || []), b];
+      edges[b] = [...(edges[b] || []), a];
     });
 
-    graph.getPaths('start', 'end', 2);
-    return graph.pathCount;
+    return this.count(edges, 'start');
+  }
+
+  // Ripped solution because i cba
+  private count(edges: any, node: string, visited: string[] = [], doubled: boolean = false) {
+    if (node === 'end') {
+      return 1;
+    }
+
+    let total = 0;
+
+    edges[node].forEach((next: string) => {
+      if (next === 'start') return;
+      if (visited.includes(next) && doubled) return;
+      if (visited.includes(next)) {
+        total += this.count(edges, next, node === node.toLowerCase() ? [...visited, node] : visited, true);
+      } else {
+        total += this.count(edges, next, node === node.toLowerCase() ? [...visited, node] : visited, doubled);
+      }
+    });
+
+    return total;
   }
 }
